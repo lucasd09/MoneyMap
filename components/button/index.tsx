df07033452f3bb3692d180899bf3f915db248fbe
dalan <lucas.dalan@harmonit.com.br>
2024-09-16 14:52:19 -0300

    feat: theme-toggle

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,7 +1,5 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
-import { cva } from "class-variance-authority"
-
 import { cn } from "@/lib/utils"
 import type { ButtonProps } from "./types"
 import { buttonVariants } from "./consts"
