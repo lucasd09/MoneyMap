da44e10497992c4761879c8792f890d03118e8e6
dalan <lucas.dalan@harmonit.com.br>
2024-09-19 11:20:02 -0300

    fix(input): adiciona value e onchange do campo

--- components/inputs/text-input/index.tsx
+++ components/inputs/text-input/index.tsx
@@ -7,22 +7,28 @@ import { BaseInput } from "@/components/commom/base-input"
 export const TextInput = <TForm extends FieldValues>(props: TextInputProps<TForm>) => {
   const { name, label, form, ...TextInputProps } = props;
 
-  const defaultValue = form.formState.defaultValues?.[name] as PathValue<TForm, Path<TForm>>;
-
-  return <FormField name={name} control={form.control} defaultValue={defaultValue} render={({ field }) => {
-    return (
-      <FormItem>
-        <FormLabel>{label}</FormLabel>
-        <FormControl>
-          <BaseInput
-            placeholder={props.placeholder}
-            type="text"
-            {...field}
-            {...TextInputProps}
-          />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    );
-  }} />
-}
+  return (
+    <FormField
+      name={name}
+      control={form.control}
+      render={({ field }) => {
+        return (
+          <FormItem>
+            <FormLabel>{label}</FormLabel>
+            <FormControl>
+              <BaseInput
+                placeholder={props.placeholder}
+                type="text"
+                {...TextInputProps}
+                value={field.value || ''}
+                onChange={field.onChange}
+                onBlur={field.onBlur}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
+    />
+  );
+};
